e639e526250f6793d371bfdda662a67376d1cd56
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2014-03-28 21:50:02 +0530

    Removed usage of async module and fixed a state-machine logic issue.

--- plugins/ml.js
+++ plugins/ml.js
@@ -4,42 +4,45 @@
 // type: `haraka -h Plugins` for documentation on how to create a plugin
 
 var outbound = require("./outbound");
-var async = require("async");
 
-exports.hook_data = function(next, connection, params) {
+exports.hook_data = function(next, connection) {
     var transaction = connection.transaction;
     transaction.parse_body = true;
+    next();
+}
+
+exports.hook_queue = function(next, connection) {
+    connection.relaying = true;
+    var transaction = connection.transaction;
     var domain = this.config.get("host_list")[0];
     var lists = this.config.get("lists.json", "json")
     var plugin = this;
-    var outnext = function (code, msg) {
-        switch (code) {
-	    case DENY:
-	        plugin.logerror("Sending mail failed: " + msg);
-	        break;
-	    case OK:
-	        plugin.loginfo("mail sent");
-	        next();
-	        break;
-	    default:
-	        plugin.logerror("Unrecognised return code from sending email: " + msg);
-	        next();
-	}
-    };
     if (transaction.mail_from.host === domain) {
+	var recipients = [];
 	transaction.message_stream.get_data(function (contents) {
 	    contents = contents.replace(/\r/g, '');
-	    async.each(transaction.rcpt_to, function(list, cb) {
-		var users = lists[list.user];
-		if (users) {
-		    async.each(users, function(user, cb2) {
-			if (user !== transaction.mail_from.address()) {
-			    outbound.send_email(transaction.mail_from, user, contents, outnext);
-			}
-		    });
-		}
+	    for (var ii=0; ii < transaction.rcpt_to.length; ii++) {
+                var list = transaction.rcpt_to[ii];
+                var users = lists[list.user];
+                for (var jj=0; jj < users.length; jj++) {
+                    var user = users[jj];
+                    if (user !== transaction.mail_from.address()) {
+	    		recipients.push(user);
+	    	    }
+	    	}
+	    }
+	    outbound.send_email(transaction.mail_from, recipients, contents, function (code, msg) {
+	    	switch (code) {
+	    	case DENY:
+	    	    plugin.logerror("Sending mail failed: " + msg);
+	    	    break;
+	    	case OK:
+	    	    plugin.loginfo("mail sent");
+	    	    break;
+	    	default:
+	    	    plugin.logerror("Unrecognised return code from sending email: " + msg);
+	    	}
 	    });
 	});
-	next();
     }
 }
